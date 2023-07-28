870cedaac3ab4da2a181bf5924cd39e476981f2a
Rodrigo Paiva <103088962+rods-paiva@users.noreply.github.com>
2023-07-28 19:25:10 -0300

    Add files via upload

---
+++ bq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## Problem 2
+bq
+rel ABC
+di 
+
+## Problem 2
+bq
+rel XYZ
+re f
+re l
+
+## Problem 3
+bq
+rel XYZ
+key #Enter the key value '10' when prompted and press Enter.
+re k #Read the record associated with the key value 10 using the 're k' command:
+re n #Read the next 2 records using the 're n' command twice:
+re n
